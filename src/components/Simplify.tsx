eba7a5fcfeb5538c272d71b022da31a139b69e05
Bada Jesutobi <jesutobi_dev@outlook.com>
2024-11-03 09:30:09 +0100

    fix:css fix

--- src/components/Simplify.tsx
+++ src/components/Simplify.tsx
@@ -5,7 +5,7 @@ import patternMobile from "../images/bg-simplify-section-mobile.svg"
 
 const Simplify : FC = () => {
   return (
-    <section className='bg-bright-red px-5 md:px-12 flex-col md:flex-row space-y-10 text-center flex justify-between lg:py-20 lg:px-28 items-center relative'>
+    <section className='bg-bright-red px-5 py-10 md:px-12 flex-col md:flex-row space-y-10 text-center flex justify-between lg:py-20 lg:px-28 items-center relative'>
         <h4 className='text-4xl text-white font-semibold max-w-[400px]'>Simplify how your team works today.</h4>
         <Link to="/" className="button-secondary">Get Started</Link>
         <div className='absolute top-0 left-0 inset-0 overflow-hidden'>
